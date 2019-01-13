b9f061bdb1185fc7b148ba7ae832e03dd4d0507f
Dreawen <dreamer_1313@bk.ru>
2019-01-13 17:10:20 +0200

    add config

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+//Base
+define('ADMIN_DIR','admin');
+define('ABS_PATH', dirname(__FILE__));
+
+//Database
+define('DB_HOST','localhost');
+define('DB_NAME','my_cms');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_CHARSET','utf8');
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 
-$a;
-// er
+//Version
+
+define('Version', '2.0.0');
+
+require_once 'config.php';
